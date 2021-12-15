dd049db743bf2bcac3887b48bc473d56b4ee9ef4
pavan <pavangurram56@gmail.com>
2021-12-15 15:32:53 +0530

    >>openModal function and closeModal functions are addded for improving code reusability<<

--- model window/modalScript.js
+++ model window/modalScript.js
@@ -3,20 +3,19 @@ const modal = document.querySelector('.modal');
 const overlay = document.querySelector('.overlay');
 const btnCloseModal = document.querySelector('.close-modal');
 const btnOpenModal = document.querySelectorAll('.show-modal');
-console.log(btnOpenModal);
-
-for (let i = 0; i < btnOpenModal.length; i++) {
-  btnOpenModal[i].addEventListener('click', function () {
-    // console.log('button clicked');
-    modal.classList.remove('hidden');
-    overlay.classList.remove('hidden');
-  });
-}
-
+//function triggered when the modal button is clicked
+const openModel = function () {
+  modal.classList.remove('hidden');
+  overlay.classList.remove('hidden');
+};
+//function will trigger when ❌ symbol and outside the model space is clicked
 const closeModal = function () {
   modal.classList.add('hidden');
   overlay.classList.add('hidden');
 };
+for (let i = 0; i < btnOpenModal.length; i++) {
+  btnOpenModal[i].addEventListener('click', openModel);
+}
 
 btnCloseModal.addEventListener('click', closeModal);
 overlay.addEventListener('click', closeModal);
